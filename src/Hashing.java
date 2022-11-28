d15e767b3f2b73c40be0e136227344b8383ab70e
Zlow <asger@bissen.dk>
2022-11-28 22:14:59 +0100

    fidilifut

---
+++ src/Hashing.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hashing {
+
+    public static String MD5Hash(String s) {
+        String result = "";
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            result = new BigInteger(1,md.digest()).toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static String SHA512Hash(String s) {
+        String result = "";
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            result = new BigInteger(1,md.digest()).toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println(Hashing.MD5Hash("hej"));
+		System.out.println(Hashing.SHA512Hash("hej"));
+	}
+}
